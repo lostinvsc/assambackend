3ff29afa19a1454178190dba7e8db34273f91544
sachin <nagar007sachin@gmail.com>
2025-05-18 07:52:24 +0530

    Update getApplicationById route.ts

--- src/app/api/getapplicationByUserId/route.ts
+++ src/app/api/getapplicationByUserId/route.ts
@@ -1,9 +1,7 @@
-import { getApplicationsByUserId } from "@/controllers/applications_controller";
+import { getApplicationsByUserId } from "../../../controllers/applications_controller";
 import { NextRequest } from "next/server";
 
 // POST request
 export const POST = async (req: NextRequest) => {
   return await getApplicationsByUserId(req);
 };
-
-
