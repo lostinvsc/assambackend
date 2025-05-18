ab84f43c2648e078d6871dc9ad36a8a03f5e2ee2
sachin <nagar007sachin@gmail.com>
2025-05-18 07:33:59 +0530

    added function for get applicationbyid applications_controller.ts

--- src/controllers/applications_controller.ts
+++ src/controllers/applications_controller.ts
@@ -101,6 +101,51 @@ export const getApplications = async (req: NextRequest): Promise<NextResponse> =
   }
 };
 
+// Get getApplicationsByUserId 
+export const getApplicationsByUserId = async (req: NextRequest): Promise<NextResponse> => {
+  if (req.method === "OPTIONS") return handleOptionsRequest();
+
+  try {
+    await connectDB();
+
+        const body = await req.json();
+
+    // Validate required fields
+    const {
+      createdBy
+    } = body;
+
+      if (!createdBy) {
+      return NextResponse.json(
+        { error: "'createdBy' is required in the request body." },
+        { status: 400, headers: corsHeaders }
+      );
+    }
+
+    const userfind=await User.findOne({userId:createdBy});
+
+    if(userfind){
+      const applications = await Application.find({createdBy});
+       return NextResponse.json(
+      { data: applications },
+      { status: 200, headers: corsHeaders }
+    );
+    }
+
+     return NextResponse.json(
+      { message: "No user with given userId found" },
+      { status: 404, headers: corsHeaders }
+    );
+
+   
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error", details: (error as Error).message },
+      { status: 500, headers: corsHeaders }
+    );
+  }
+};
+
 // Update Application Status
 export const updateApplicationStatus = async (req: NextRequest): Promise<NextResponse> => {
   if (req.method === "OPTIONS") return handleOptionsRequest();
