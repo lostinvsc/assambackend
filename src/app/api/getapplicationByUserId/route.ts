7d8991a3a6d836180dce41f15960f173cf2d8026
sachin <nagar007sachin@gmail.com>
2025-05-18 07:32:03 +0530

    Add files via upload

---
+++ src/app/api/getapplicationByUserId/route.ts
@@ -0,0 +1,9 @@
+import { getApplicationsByUserId } from "@/controllers/applications_controller";
+import { NextRequest } from "next/server";
+
+// POST request
+export const POST = async (req: NextRequest) => {
+  return await getApplicationsByUserId(req);
+};
+
+
